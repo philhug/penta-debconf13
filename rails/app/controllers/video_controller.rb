48bd922f9ae1c05ea72671539710e2e33077ee31
Ben Hutchings <ben@decadent.org.uk>
2011-07-20 00:42:51 +0200

    Lock video events and recordings while under review
    Recordings must already be claimed/locked before review and can later
    be unclaimed/unlocked by the holder or an admin.
    
    We need to do the same for events, so that transcoding for an event
    with multiple recordings is not started before all recordings are
    associated with it.
    
    Provide multiple 'finish' buttons on the review form which will unlock
    the event and/or recording as appropriate.  Also add 'unclaim' actions
    to the event list.

--- rails/app/controllers/video_controller.rb
+++ rails/app/controllers/video_controller.rb
@@ -1,6 +1,9 @@
 class VideoController < ApplicationController
   before_filter :init
   around_filter :update_last_login
+  before_filter :check_event_may_lock, :only=>[:link_to_event]
+  before_filter :check_event_lock, :only=>[:ev_rec_data]
+  before_filter :check_event_may_unlock, :only=>[:unlock_event]
   before_filter :check_recording_lock, :only=>[:associate, :link_to_event, :mark_for_deletion]
   before_filter :check_target_file_lock, :only=>[:rate_file, :vtf_rating]
 
@@ -27,6 +30,10 @@ class VideoController < ApplicationController
     if not @ve.event_base_name
       @ve.event_base_name = sprintf('%03d_',@event.event_id) + @event.title.gsub(/[^\s\w\d\.\+\_\-]/, '').gsub(/\s+/,'_').gsub(/\.$/,'')
     end
+    if not @ve.locked_by
+      @ve.locked_by = POPE.user.person_id
+    end
+    @ve.write
     @ver = Video_event_recording.new(:event_id => @event.event_id, :recording_id => @rec.id)
   end
 
@@ -39,7 +46,14 @@ class VideoController < ApplicationController
     params[:video_event_recording][:start_time] = "00:00:00" if params[:video_event_recording][:start_time].empty?
     params[:video_event_recording][:end_time] = Video_recording.select_single({:id => params[:video_recording][:id]}).recording_duration if params[:video_event_recording][:end_time].empty?
 
-    ve = write_row(Video_event, params[:video_event], {:preset => {:event_id => params[:video_event_recording][:event_id]}});
+    if params[:finish] || params[:finish_recording]
+      params[:video_recording][:locked_by] = nil
+    end
+    if params[:finish] || params[:finish_event]
+      params[:video_event][:locked_by] = nil
+    end
+
+    ve = write_row(Video_event, params[:video_event], {:preset => {:event_id => params[:video_event_recording][:event_id]}})
     ver = write_row(Video_event_recording, params[:video_event_recording])
     rec = write_row(Video_recording, params[:video_recording], {:preset => {:conference_id => @current_conference.conference_id}})
     redirect_to( :action => 'index' )
@@ -110,6 +124,11 @@ class VideoController < ApplicationController
     @events = View_event.select({:conference_id => @current_conference.conference_id, :translated => @current_language}, {:order => [:conference_day, :start_time]})
   end
 
+  def unlock_event
+    write_row(Video_event, {:event_id => params[:event_id], :locked_by => nil})
+    redirect_to(:action => "list_events")
+  end
+
   def toggle_lock_recording
     vr = Video_recording.select_single({:id => params[:id], :conference_id => @current_conference.conference_id})
     raise "Locking unknown recording" if !vr
@@ -171,6 +190,24 @@ class VideoController < ApplicationController
     @current_conference = Conference.select_single(:conference_id => POPE.user.current_conference_id)
     @current_language = POPE.user.current_language
     @video_base = 'file:///video'
+  end
+
+  def check_event_may_lock
+    id = params[:event_id]
+    events = Video_event.select({:event_id => id})
+    return (events.length == 0 || events[0].locked_by == nil ||
+            events[0].locked_by == POPE.user.person_id)
+  end
+
+  def check_event_lock
+    id = params[:video_event_recording][:event_id]
+    return Video_event.select_single({:event_id => id}).locked_by == POPE.user.person_id
+  end
+
+  def check_event_may_unlock
+    id = params[:event_id]
+    return (Video_event.select_single({:event_id => id}).locked_by == POPE.user.person_id ||
+            POPE.permission?("video_admin"))
   end
 
   def check_recording_lock
