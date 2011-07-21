7ae9b98af9e7dba1cd885083745981960ddfca00
Gunnar Wolf <gwolf@gwolf.org>
2011-07-21 13:33:33 +0200

    Fixed an angry goddess and a missing translation

--- sql/maintenance/debconf11_add_sim_cards.sql
+++ sql/maintenance/debconf11_add_sim_cards.sql
@@ -1,1 +1,2 @@
 ALTER TABLE debconf.dc_conference_person ADD COLUMN has_sim_card boolean NOT NULL DEFAULT FALSE;
+INSERT INTO ui_message_localized (ui_message, translated, name) VALUES ('table::dc_conference_person::has_sim_card', 'en', 'Has received a SIM card?');
