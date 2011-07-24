314e9247e1df1b409f61a4d76d24dadd42730731
Gunnar Wolf <gwolf@gwolf.org>
2011-07-24 15:19:09 +0200

    Reflecting some missing permissions which have been granted

---
+++ sql/maintenance/debconf11_grants_for_video.sql
@@ -0,0 +1,4 @@
+-- As requested by benh for videoteam work
+GRANT SELECT, INSERT, UPDATE, DELETE on video_target_file TO dc7video;
+GRANT SELECT, UPDATE ON video_target_file TO pentabarf;
+GRANT SELECT, UPDATE ON video_target_file_id_seq1 TO dc7video;
