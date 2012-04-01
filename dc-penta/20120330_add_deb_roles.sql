b4319f89d585903b8ab3e70e0e14bb205310906f
Gunnar Wolf <gwolf@gwolf.org>
2012-03-31 23:49:06 -0600

    Last set of changes for debconf_role. Now it even works!

--- dc-penta/20120330_add_deb_roles.sql
+++ dc-penta/20120330_add_deb_roles.sql
@@ -11,6 +11,8 @@ CREATE TABLE base.dc_debconf_role (
 );
 ALTER TABLE base.dc_participant_mapping ALTER COLUMN person_type_id DROP NOT NULL;
 ALTER TABLE base.dc_participant_mapping ADD COLUMN debconf_role_id integer;
+ALTER TABLE base.dc_conference_person ADD COLUMN debconf_role_id integer;
+ALTER TABLE base.dc_conference_person ADD COLUMN debian_role_id integer;
 
 -- Add the public tables
 CREATE TABLE debconf.dc_debian_role () INHERITS (base.dc_debian_role);
@@ -25,12 +27,35 @@ ALTER TABLE ONLY debconf.dc_participant_mapping
   ADD CONSTRAINT dc_participant_mapping_debconf_role_id_fkey
   FOREIGN KEY (debconf_role_id)
   REFERENCES debconf.dc_debconf_role(debconf_role_id);
+ALTER TABLE ONLY debconf.dc_conference_person
+  ADD CONSTRAINT dc_conference_person_debian_role_id_fkey
+  FOREIGN KEY (debian_role_id)
+  REFERENCES debconf.dc_debian_role(debian_role_id);
+ALTER TABLE ONLY debconf.dc_conference_person
+  ADD CONSTRAINT dc_conference_person_debconf_role_id_fkey
+  FOREIGN KEY (debconf_role_id)
+  REFERENCES debconf.dc_debconf_role(debconf_role_id);
 ALTER TABLE ONLY debconf.dc_participant_mapping
   ADD CHECK ((person_type_id IS NOT NULL) OR (debconf_role_id IS NOT NULL));
 
 -- Add the logging tables
 CREATE TABLE log.dc_debian_role () INHERITS (base.logging, base.dc_debian_role);
 CREATE TABLE log.dc_debconf_role () INHERITS (base.logging, base.dc_debconf_role);
+
+-- Adjust debconf.dc_view_participant with the changes (and with DebConf12)
+DROP VIEW debconf.dc_view_participant;
+CREATE VIEW debconf.dc_view_participant AS
+       SELECT dc_participant_mapping.participant_category_id,
+       	      dc_participant_mapping.participant_mapping_id,
+	      dc_participant_mapping.person_type_id,
+	      dc_participant_mapping.debconf_role_id,
+	      dc_participant_category.participant_category
+       FROM debconf.dc_participant_mapping
+       JOIN debconf.dc_participant_category USING (participant_category_id)
+       WHERE dc_participant_category.conference_id = 6
+       ORDER BY dc_participant_mapping.person_type_id,
+       	      dc_participant_mapping.participant_category_id;
+
 
 -- Insert the data
 --
@@ -60,6 +85,7 @@ INSERT INTO debconf.dc_accomodation (accom_id, conference_id, accom) VALUES (17,
 INSERT INTO debconf.dc_accomodation (accom_id, conference_id, accom) VALUES (18, 6, 'Regular room');
 INSERT INTO debconf.dc_accomodation (accom_id, conference_id, accom) VALUES (19, 6, 'I will arrange my own accomodation');
 
+INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (0, ' --- Please select one --- ');
 INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (1, 'Debian Developer');
 INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (2, 'Debian Developer (non-uploading)');
 INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (3, 'Debian Maintainer');
@@ -68,6 +94,7 @@ INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (5, 'Not
 INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (6, 'Accompanying a Debian participant');
 INSERT INTO debconf.dc_debian_role (debian_role_id, description) VALUES (7, 'None');
 
+INSERT INTO debconf.dc_debconf_role (debconf_role_id, description) VALUES (0, ' --- Please select one --- ');
 INSERT INTO debconf.dc_debconf_role (debconf_role_id, description) VALUES (1, 'Regular attendee');
 INSERT INTO debconf.dc_debconf_role (debconf_role_id, description) VALUES (2, 'Organizer');
 INSERT INTO debconf.dc_debconf_role (debconf_role_id, description) VALUES (3, 'Volunteer');
